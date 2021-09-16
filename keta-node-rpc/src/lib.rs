95f9d6341911428b319d5d9372272d91bae1abc2
gbaranski <root@gbaranski.com>
2021-09-16 20:04:05 +0200

    feat(node-rpc): add serve_http

--- keta-node-rpc/src/lib.rs
+++ keta-node-rpc/src/lib.rs
@@ -4,9 +4,9 @@ use keta_core::block::HashedBlock;
 use keta_core::transaction::SignedTransaction;
 
 #[derive(Debug, thiserror::Error)]
-pub enum Error{
-    #[error("internal: {0}")]
-    Internal(String),
+pub enum Error {
+    #[error("world: {0}")]
+    World(String),
 }
 
 impl From<Error> for jsonrpc_core::Error {
@@ -18,7 +18,6 @@ impl From<Error> for jsonrpc_core::Error {
         }
     }
 }
-
 
 #[rpc]
 pub trait Rpc {
@@ -36,6 +35,26 @@ pub trait Rpc {
 }
 
 #[cfg(feature = "client-http")]
-pub async fn connect_http(url: &str) -> Result<RpcClient, jsonrpc_core_client::Error> {
+pub async fn connect_http(url: &str) -> Result<RpcClient, jsonrpc_core_client::RpcError> {
     jsonrpc_core_client::transports::http::connect(url).await
+}
+
+#[cfg(feature = "server-http")]
+pub async fn serve_http(
+    server: impl Rpc,
+    address: &std::net::SocketAddr,
+) -> Result<(), std::io::Error> {
+    use jsonrpc_http_server::ServerBuilder;
+
+    let mut io = jsonrpc_core::IoHandler::new();
+    io.extend_with(server.to_delegate());
+    let server = ServerBuilder::new(io)
+        .threads(1)
+        .event_loop_executor(tokio::runtime::Handle::current())
+        .start_http(address)?;
+    let task = tokio::task::spawn(async move {
+        server.wait();
+        Ok(())
+    });
+    task.await.unwrap()
 }
